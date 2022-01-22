c478d3a94854c95f4fcece74e442545762820b4b
ENRICO TOMASI <Cinicharry@gmail.com>
2022-01-22 11:46:16 +0100

    Score implemented

--- src/main/java/Game/Elements/Score.java
+++ src/main/java/Game/Elements/Score.java
@@ -1,4 +1,20 @@
 package Game.Elements;
 
 public class Score {
+    int stepsTaken;
+    int enemiesKilled;
+    int difficultySet;
+    double timeElapsed;
+
+    /**
+     * Inizializza un'istanza del punteggio per la partita corrente
+     *
+     * @param difficultySetByGame difficoltà impostata dal gioco
+     */
+    public Score(int difficultySetByGame) {
+        stepsTaken = 0;
+        enemiesKilled = 0;
+        difficultySet = difficultySetByGame;
+        timeElapsed = 0.0;
+    }
 }
